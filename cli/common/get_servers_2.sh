3e9896f82e0e94d24004214c9a532a56e08dff1b
Javier <javier@staff-net-vpn-dhcp-8051.intern.ethz.ch>
2023-09-13 16:12:05 +0200

    servers-list: operative version

--- cli/common/get_servers_2.sh
+++ cli/common/get_servers_2.sh
@@ -1,18 +1,60 @@
 #!/bin/bash
 
 CLI_PATH=$1
-server_list=$2
+SERVER_LIST=$2
 hostname=$3
+username=$4
+
+test_ssh_access() {
+    username="$1"
+    server="$2"
+    /usr/bin/ssh -q -o BatchMode=yes -o ConnectTimeout=5 "$username@$server" exit
+    if [ $? -eq 0 ]; then
+        return 0  # SSH access is successful
+    else
+        return 1  # SSH access failed
+    fi
+}
 
 # Declare global variables
 declare -g servers_family_list=""
 declare -g servers_family_list_string=""
 
-#get booked machines
-servers=$(sudo $CLI_PATH/common/get_booking_system_servers_list | tail -n +2)
+# Read server names from the SERVER_LIST file into an array
+#mapfile -t servers < $SERVER_LIST
+
+echo "New server list:"
+echo $SERVER_LIST
 
 # Convert string to an array
-servers=($servers)
+SERVER_LIST=($SERVER_LIST)
+
+#get booked machines
+servers_old=$(sudo $CLI_PATH/common/get_booking_system_servers_list | tail -n +2)
+
+echo "Servers old:"
+echo $servers_old
+
+#Loop through the server list and test SSH access
+#servers=""
+servers=()
+for server in "${SERVER_LIST[@]}"; do
+    echo "Testing server $server"
+    if test_ssh_access "$username" "$server"; then
+        #servers+=" $server"
+        echo "$server = Yes!"
+        servers+=("$server") 
+    fi
+done
+
+# Convert string to an array
+servers_old=($servers_old)
+#servers=($servers)
+
+#echo "servers_old: $servers_old"
+#echo "servers: $servers"
+
+#exit
 
 # We only show likely servers (i.e., alveo-u55c)
 server_family="${hostname%???}"
@@ -26,6 +68,10 @@ do
         servers_family_list+=("$i") 
     fi
 done
+
+echo $servers_family_list
+
+exit
 
 #convert to string and remove the leading delimiter (:2)
 servers_family_list_string=$(printf ", %s" "${servers_family_list[@]}")
