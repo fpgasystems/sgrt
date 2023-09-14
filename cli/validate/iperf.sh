d98e3de2b6db00e9da0b72c6c4ee6d10446021c5
Javier <javier@staff-net-vpn-dhcp-0727.intern.ethz.ch>
2023-09-14 06:21:02 +0200

    Using new get_servers

--- cli/validate/iperf.sh
+++ cli/validate/iperf.sh
@@ -6,6 +6,9 @@ normal=$(tput sgr0)
 #constants
 CLI_PATH="$(dirname "$(dirname "$0")")"
 
+#combine ACAP and FPGA lists removing duplicates
+SERVER_LIST=$(sort -u $CLI_PATH/constants/ACAP_SERVERS_LIST /$CLI_PATH/constants/FPGA_SERVERS_LIST)
+
 echo ""
 
 #setup keys
@@ -14,6 +17,9 @@ eval "$CLI_PATH/common/ssh_key_add"
 #get hostname
 url="${HOSTNAME}"
 hostname="${url%%.*}"
+
+#get username
+username=$USER
 
 #inputs
 flags=$@
@@ -39,17 +45,40 @@ iperf -s -B $hostname-mellanox-0 -D $udp_server
 echo ""
 
 #get booked machines
-servers=$(sudo $CLI_PATH/common/get_booking_system_servers_list | tail -n +2)
-echo ""
+#servers=$(sudo $CLI_PATH/common/get_booking_system_servers_list | tail -n +2)
+
+result=$($CLI_PATH/common/get_servers $CLI_PATH "$SERVER_LIST" $hostname $username)
+servers_family_list=$(echo "$result" | sed -n '1p' | sed -n '1p') #
+#servers_family_list_string=$(echo "$result" | sed -n '2p' | sed -n '1p')
 
 #convert string to an array
-servers=($servers)
+servers_family_list=($servers_family_list)
+
+#echo "Test servers_family_list"
+#echo ""
+#for i in "${servers_family_list[@]}"; do
+#    echo "$i"
+#done
+
+#echo ""
+
+#convert string to an array
+#servers=($servers)
+
+#echo "Test servers"
+#echo ""
+#for i in "${servers[@]}"; do
+#    echo "$i"
+#done
+
+#exit
+
+
 
 #running iperf on remote machines
 echo "${bold}Running iperf on remote server/s:${normal}"
 echo ""
-for i in "${servers[@]}"
-do
+for i in "${servers_family_list[@]}"; do #servers
     if [ "$i" != "$hostname" ]; then
         echo "iperf -c $hostname-mellanox-0 -B $i-mellanox-0 $flags"
         echo ""
