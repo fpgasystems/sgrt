72d20cabd308641c40820f20dcba60ff55f6036a
Javier <javier@staff-net-vpn-dhcp-0777.intern.ethz.ch>
2023-09-14 12:02:11 +0200

    Quering echo message add

--- cli/program/vitis.sh
+++ cli/program/vitis.sh
@@ -93,10 +93,13 @@ if [ "$flags" = "" ]; then
     fi
     #get_servers
     echo ""
+    echo "${bold}Quering remote servers with ssh:${normal}"
     result=$($CLI_PATH/common/get_servers $CLI_PATH "$SERVER_LIST" $hostname $username)
     servers_family_list=$(echo "$result" | sed -n '1p' | sed -n '1p')
     servers_family_list_string=$(echo "$result" | sed -n '2p' | sed -n '1p')
     num_remote_servers=$(echo "$servers_family_list" | wc -w)
+    echo ""
+    echo "Done!"
     echo ""
     #deployment_dialog
     deploy_option="0"
@@ -167,10 +170,14 @@ else
         echo ""
     fi
     #get_servers
+    echo ""
+    echo "${bold}Quering remote servers with ssh:${normal}"
     result=$($CLI_PATH/common/get_servers $CLI_PATH "$SERVER_LIST" $hostname $username)
     servers_family_list=$(echo "$result" | sed -n '1p' | sed -n '1p')
     servers_family_list_string=$(echo "$result" | sed -n '2p' | sed -n '1p')
     num_remote_servers=$(echo "$servers_family_list" | wc -w)
+    echo ""
+    echo "Done!"
     echo ""
     #deployment_dialog (forgotten mandatory 3)
     if [ "$deploy_option_found" = "0" ]; then
