3166f5015547f13091d1384fb1157c241033ce1f
Javier <javier@staff-net-als-0558.intern.ethz.ch>
2023-09-18 12:35:23 +0200

    Using new get_servers method

--- cli/set/keys.sh
+++ cli/set/keys.sh
@@ -6,12 +6,34 @@ normal=$(tput sgr0)
 #constants
 CLI_PATH="$(dirname "$(dirname "$0")")"
 
-#inputs
-read -a flags <<< "$@"
+#combine ACAP and FPGA lists removing duplicates
+SERVER_LIST=$(sort -u $CLI_PATH/constants/ACAP_SERVERS_LIST /$CLI_PATH/constants/FPGA_SERVERS_LIST)
+
+#get hostname
+url="${HOSTNAME}"
+hostname="${url%%.*}"
+
+#get username
+username=$USER
 
 echo ""
 echo "${bold}sgutil set keys${normal}"
-echo ""
 
-# setup keys
-$CLI_PATH/common/ssh_key_add
+#get booked servers accessible with ssh
+echo ""
+echo "${bold}Quering remote servers with ssh:${normal}"
+result=$($CLI_PATH/common/get_servers $CLI_PATH "$SERVER_LIST" $hostname $username)
+servers_family_list=$(echo "$result" | sed -n '1p' | sed -n '1p')
+servers_family_list_string=$(echo "$result" | sed -n '2p' | sed -n '1p')
+num_remote_servers=$(echo "$servers_family_list" | wc -w)
+
+#check on number of servers
+if [ "$num_remote_servers" -eq 0 ]; then
+    echo ""
+    echo "Please, verify that you can ssh the targeted remote servers."
+    echo ""
+    exit
+fi
+
+#setup keys
+$CLI_PATH/common/ssh_key_add $CLI_PATH "${servers_family_list[@]}"
