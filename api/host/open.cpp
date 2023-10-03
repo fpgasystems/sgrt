bf00f2d4addd704aa91b2dd2611469b597ffb22a
Javier <javier@staff-net-vpn-dhcp-2453.intern.ethz.ch>
2023-10-03 14:06:45 +0200

    Working version

--- api/host/open.cpp
+++ api/host/open.cpp
@@ -1,16 +1,18 @@
 #include "open.hpp" // Include the header file
+#include "../device.hpp"
 
 xrt::device host::open(const std::string& device_bdf) {
     
-    xrt::device device;
+    xrt::device xrt_device;
+    device::vitis device;
 
     if (device_bdf.empty()) {
-        device = xrt::device(0);
+        xrt_device = xrt::device(0);
     } else {
         std::cout << "\nOpening the device: " << device_bdf << "\n" << std::endl;
-        device = xrt::device(device_bdf);
+        xrt_device = xrt::device(device_bdf);
     }
     
-    return device;
+    return xrt_device;
 
 }
