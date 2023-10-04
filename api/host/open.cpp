8a015d447db258f84949b53d7a444dbd5c500feb
Javier <javier@staff-net-vpn-dhcp-2700.intern.ethz.ch>
2023-10-04 07:41:34 +0200

    Cleanup verified

--- api/host/open.cpp
+++ api/host/open.cpp
@@ -1,46 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-
 #include "open.hpp" // Include the header file
 #include "../device.hpp"
 #include "../common/sgutil_get.hpp"
-
-/* std::string sgutil_get(int device_index, int columnNumber) {
-    
-    std::string filename = "/opt/sgrt/cli/devices_acap_fpga";
-    
-    std::ifstream inputFile(filename);
-
-    if (!inputFile) {
-        std::cerr << "Error opening file." << std::endl;
-        return ""; // Return an empty string to indicate an error
-    }
-
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        // Split the line into columns based on whitespace
-        std::istringstream iss(line);
-        int column1;
-        std::string value;
-
-        if (iss >> column1) {
-            if (column1 == device_index) {
-                for (int i = 1; i <= columnNumber; ++i) {
-                    if (!(iss >> value)) {
-                        break; // Return an empty string if there are fewer than columnNumber columns
-                    }
-                }
-                inputFile.close();
-                return value; // Return the value in the specified column
-            }
-        }
-    }
-
-    inputFile.close();
-    return ""; // Return an empty string if no matching value is found
-} */
 
 device::vitis host::open(const std::string& device_bdf) {
 
