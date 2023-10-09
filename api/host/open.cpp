069bcb6c8d9a74e83ec84b244e6aa8f1606452fd
Javier <javier@staff-net-vpn-dhcp-2830.intern.ethz.ch>
2023-10-09 13:59:52 +0200

    loading xclbin validated

--- api/host/open.cpp
+++ api/host/open.cpp
@@ -40,7 +40,7 @@ device::vitis host::open(const std::string& device_index, const std::string& bin
     int IP = 7;
     int MAC = 8;
     int PLATFORM = 9;
-    
+
     // XRT and SGRT objects
     xrt::device xrt_device;
     device::vitis device;
@@ -50,6 +50,10 @@ device::vitis host::open(const std::string& device_index, const std::string& bin
         xrt_device = xrt::device(0);
     } else {
         
+        // constants
+        std::string current_uuid_str="00000000-0000-0000-0000-000000000000";
+        std::string new_uuid_str="00000000-0000-0000-0000-000000000000";
+
         // get device index
         device.device_index = std::stoi(device_index);
 
@@ -70,13 +74,31 @@ device::vitis host::open(const std::string& device_index, const std::string& bin
         xrt_device = xrt::device(bdf);
 
         // load xclbin
-        auto uuid = xrt_device.load_xclbin(binaryFile);
+        xrt::xclbin new_xclbin = xrt::xclbin(binaryFile);
+        auto new_uuid = new_xclbin.get_uuid();
+        auto current_uuid = xrt_device.get_xclbin_uuid();
+        //auto uuid = xrt_device.load_xclbin(binaryFile);
+        // convert to str
+        current_uuid_str = current_uuid.to_string();
+        new_uuid_str = new_uuid.to_string();
+        // compare and load
+        std::cout << "Device " << device_index << " - Loading xclbin: "<< new_uuid_str << std::endl;
+        if (current_uuid_str == "00000000-0000-0000-0000-000000000000" || current_uuid_str != new_uuid_str){
+            // load new xclbin
+            //std::cout << "\nLoading xclbin: "<< new_uuid_str << std::endl;
+            auto uuid = xrt_device.load_xclbin(binaryFile);
+            device.uuid = new_uuid_str;
+        } else {
+            // requested xclbin was already loaded
+            device.uuid = current_uuid;
+        }
+        std::cout << "Done!\n" << std::endl;
 
         // save uuid
-        auto current_uuid = xrt_device.get_xclbin_uuid();
+        //auto current_uuid = xrt_device.get_xclbin_uuid();
 
-        std::string current_uuid_str=current_uuid.to_string(); //"00000000-0000-0000-0000-000000000000";
-        device.uuid = current_uuid_str;
+        //std::string current_uuid_str=current_uuid.to_string(); //"00000000-0000-0000-0000-000000000000";
+        //device.uuid = new_uuid_str; //current_uuid.to_string();
 
     }
 
