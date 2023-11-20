55ac7e3a1541c4a51ff2d3585a17b3ec9ed38cb7
Javier <javier@staff-net-vpn-dhcp-5110.intern.ethz.ch>
2023-11-20 09:50:22 +0100

    Check if already defined

--- cli/set/license.sh
+++ cli/set/license.sh
@@ -12,5 +12,22 @@ mapfile -t lines < "$file_path"
 # Join the array elements with ':' to create the desired format
 XILINXD_LICENSE_FILE=$(IFS=:; echo "${lines[*]}")
 
+
+# Set the environment variable
+if printenv | grep -q XILINXD_LICENSE_FILE; then
+    echo ""
+    echo "The ${bold}XILINXD_LICENSE_FILE=$XILINXD_LICENSE_FILE${normal} is already defined!"
+    echo ""
+else
+    export XILINXD_LICENSE_FILE
+    echo ""
+    echo "The following ${bold}license servers${normal} have been defined:"
+    echo ""
+    cat $file_path
+    echo ""
+    echo ""
+fi
+
 # Print the result or use it as needed
-export XILINXD_LICENSE_FILE
+#export XILINXD_LICENSE_FILE
+
