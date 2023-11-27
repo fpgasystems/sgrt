bc27fdf7f0ebba54f9415e0939aa9c8553b1dfc8
Javier <javier@staff-net-vpn-dhcp-4663.intern.ethz.ch>
2023-11-27 13:56:39 +0100

    Cleanup comment

--- cli/program/reset.sh
+++ cli/program/reset.sh
@@ -23,9 +23,19 @@ if [ "$acap" = "0" ] && [ "$fpga" = "0" ]; then
 fi
 
 #check on valid XRT version
-if [ ! -d $XRT_PATH ]; then
+#if [ ! -d $XRT_PATH ]; then
+#    echo ""
+#    echo "Please, source a valid XRT and Vitis version for ${bold}$hostname!${normal}"
+#    echo ""
+#    exit 1
+#fi
+
+#check on valid XRT and Vivado version
+xrt_version=$($CLI_PATH/common/get_xilinx_version xrt)
+
+if [ -z "$(echo $xrt_version)" ]; then
     echo ""
-    echo "Please, source a valid XRT and Vitis version for ${bold}$hostname!${normal}"
+    echo "Please, source a valid XRT version for ${bold}$hostname!${normal}"
     echo ""
     exit 1
 fi
