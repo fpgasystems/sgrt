553a2ae6224bf0682d5b28419d212bab1ad805d3
Javier <javier@staff-net-vpn-dhcp-4663.intern.ethz.ch>
2023-11-28 05:31:26 +0100

    Should not be available on hacc-build-01

--- cli/get/workflow.sh
+++ cli/get/workflow.sh
@@ -7,6 +7,20 @@ normal=$(tput sgr0)
 CLI_PATH="$(dirname "$(dirname "$0")")"
 DEVICES_LIST="$CLI_PATH/devices_acap_fpga"
 
+#get hostname
+url="${HOSTNAME}"
+hostname="${url%%.*}"
+
+#check on ACAP or FPGA servers (server must have at least one ACAP or one FPGA)
+acap=$($CLI_PATH/common/is_acap $CLI_PATH $hostname)
+fpga=$($CLI_PATH/common/is_fpga $CLI_PATH $hostname)
+if [ "$acap" = "0" ] && [ "$fpga" = "0" ]; then
+    echo ""
+    echo "Sorry, this command is not available on ${bold}$hostname!${normal}"
+    echo ""
+    exit
+fi
+
 #check on DEVICES_LIST
 source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST"
 
