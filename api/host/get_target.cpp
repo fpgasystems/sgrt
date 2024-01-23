4e1da2864fe0e065bdae66c61db424afad60d9c2
Javier Moya <jmoya@hey.com>
2024-01-23 12:52:13 +0100

    Returning hw when empty works

--- api/host/get_target.cpp
+++ api/host/get_target.cpp
@@ -7,28 +7,15 @@
 
 std::string host::get_target() {
 
-
-    //host::config config;
-
-    //config.project_path = project_path;
-    //config.XCL_EMULATION_MODE = xcl_emulation_mode;
-    
-    //std::cout << "project_path is:" << config.project_path << std::endl;
-
-
     const char* xclEmulationModeChar = std::getenv("XCL_EMULATION_MODE");
     std::string XCL_EMULATION_MODE = "";
     if (xclEmulationModeChar != nullptr) {
         XCL_EMULATION_MODE = xclEmulationModeChar;
-
         // ...
+    } else if (XCL_EMULATION_MODE.empty()) {
+        XCL_EMULATION_MODE = "hw";
     }
 
-
-
     return XCL_EMULATION_MODE;
-
-    //return config;
-    
 
 }
