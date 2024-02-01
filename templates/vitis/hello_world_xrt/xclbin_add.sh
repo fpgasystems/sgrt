4223c537e8d3d1e8796aa33ba2b8c16a0d2b62cc
Javier Moya <jmoya@hey.com>
2024-02-01 15:59:42 +0100

    Using TEMPLATES_PATH env variable

--- templates/vitis/hello_world_xrt/xclbin_add.sh
+++ templates/vitis/hello_world_xrt/xclbin_add.sh
@@ -4,20 +4,25 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 #constants
-MY_PROJECTS_PATH="$(dirname "$(dirname "$0")")"
-TEMPLATE="vadd"
-#MY_PROJECTS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECTS_PATH)
+MY_PROJECT_PATH="$(dirname "$(dirname "$0")")"
+
+#get template (TEMPLATES_PATH is an environment variable)
+TEMPLATE="$TEMPLATES_PATH/vitis/hello_world_xrt/src/xclbin/vadd.cpp" 
+
+echo $TEMPLATE
+
+#MY_PROJECT_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECT_PATH)
 #WORKFLOW="vitis"
 #TEMPLATE_NAME="hello_world_xrt"
 
 # create my_projects directory
-#DIR="$MY_PROJECTS_PATH"
+#DIR="$MY_PROJECT_PATH"
 #if ! [ -d "$DIR" ]; then
 #    mkdir ${DIR}
 #fi
 
 # create vitis directory
-#VITIS_DIR="$MY_PROJECTS_PATH/$WORKFLOW"
+#VITIS_DIR="$MY_PROJECT_PATH/$WORKFLOW"
 #if ! [ -d "$VITIS_DIR" ]; then
 #    mkdir ${VITIS_DIR}
 #fi
@@ -35,7 +40,7 @@ while true; do
     #if  [[ $xclbin_name == validate_* ]] || [[ $xclbin_name == "test" ]]; then
     #    xclbin_name=""
     #fi
-    XCLBIN="$MY_PROJECTS_PATH/src/xclbin/$xclbin_name.cpp"
+    XCLBIN="$MY_PROJECT_PATH/src/xclbin/$xclbin_name.cpp"
     if ! [ -e "$XCLBIN" ]; then
         break
     fi
@@ -43,7 +48,8 @@ done
 #echo ""
 
 #copy template
-cp $MY_PROJECTS_PATH/src/xclbin/$TEMPLATE.cpp $MY_PROJECTS_PATH/src/xclbin/$xclbin_name.cpp
+#cp $MY_PROJECT_PATH/src/xclbin/$TEMPLATE.cpp $MY_PROJECT_PATH/src/xclbin/$xclbin_name.cpp
+cp $TEMPLATE $MY_PROJECT_PATH/src/xclbin/$xclbin_name.cpp
 
 echo ""
 echo "The XCLBIN ${bold}$xclbin_name${normal} has been added!"
