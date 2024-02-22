cbc01c836780138631876cf6dcedf09a627398b7
Javier Moya <jmoya@hey.com>
2024-02-22 10:00:18 +0100

    Cleanup comments

--- cli/validate/coyote.sh
+++ cli/validate/coyote.sh
@@ -185,31 +185,14 @@ echo ""
 echo "${bold}Please, choose your configuration:${normal}" # this refers to a software (sw/examples) configuration
 echo ""
 PS3=""
-select config in perf_host perf_rdma_host gbm_dtrees #perf_host perf_fpga gbm_dtrees hyperloglog perf_dram perf_hbm perf_mem perf_rdma perf_rdma_host perf_rdma_card perf_tcp rdma_regex service_aes service_reconfiguration
+select config in perf_host perf_rdma_host gbm_dtrees
 do
     case $config in
-        perf_host) break;;                  #1
-        #perf_fpga) break;;                  #2
-        #perf_mem) break;;                   #7
+        perf_host) break;;
         perf_rdma_host) break;;
-        gbm_dtrees) break;;                 #3
-        #hyperloglog) break;;                #4
+        gbm_dtrees) break;;
     esac
 done
-# perf_host) break;;                  #1
-# perf_fpga) break;;                  #2
-# gbm_dtrees) break;;                 #3
-# hyperloglog) break;;                #4
-# perf_dram) break;;                  #5
-# perf_hbm) break;;                   #6
-# perf_mem) break;;                   #7
-# perf_rdma) break;;                  #7  #8
-# perf_rdma_host) break;;             #8  #9
-# perf_rdma_card) break;;             #9  #10
-# perf_tcp) break;;                   #10 #11
-# rdma_regex) break;;                 #11 #12
-# service_aes) break;;                #12 #13
-# service_reconfiguration) break;;    #13 #14
 
 #get device_name
 device_name=$($CLI_PATH/get/get_fpga_device_param $device_index device_name)
@@ -218,41 +201,12 @@ device_name=$($CLI_PATH/get/get_fpga_device_param $device_index device_name)
 platform=$($CLI_PATH/get/get_fpga_device_param $device_index platform)
 FDEV_NAME=$(echo "$platform" | cut -d'_' -f2)
 
-# adjust perf_mem validation
-#if [ "$config" = "perf_mem" ]; then
-#    case "$FDEV_NAME" in
-#        u250) 
-#            config_hw="perf_dram"
-#            ;;
-#        u280) 
-#            config_hw="perf_hbm"
-#            ;;
-#        u50)
-#            config_hw="perf_hbm"
-#            ;;
-#        u55c)
-#            config_hw="perf_hbm"
-#            ;; 
-#        *)
-#            echo ""
-#            echo "Unknown device name."
-#            echo ""
-#        ;;  
-#    esac
-#else
-#    config_hw=$config
-#fi
-
 # map sw/hw configurations
 case "$config" in
     perf_host) 
         config_hw=$config
         config_sw=$config
         ;;
-    #perf_fpga) 
-    #    config_hw=$config
-    #    config_sw=$config
-    #    ;;
     perf_rdma_host)
         config_hw=$config
         config_sw="perf_rdma"
@@ -261,10 +215,6 @@ case "$config" in
         config_hw=$config
         config_sw=$config
         ;;
-    #hyperloglog)
-    #    config_hw=$config
-    #    config_sw=$config
-    #    ;; 
     *)
         echo ""
         echo "Unknown config name."
@@ -331,19 +281,6 @@ if ! [ -d "$DIR" ]; then
             echo "const int EN_WB = 1;" >> config_shell.hpp
             echo "const int N_REGIONS = 1;" >> config_shell.hpp
             ;;
-        #perf_hbm)
-        #    echo "const int N_REGIONS = 4;" > config_shell.hpp
-        #    echo "const int EN_HLS = 0;" >> config_shell.hpp
-        #    echo "const int EN_STRM = 0;" >> config_shell.hpp
-        #    echo "const int EN_MEM = 1;" >> config_shell.hpp
-        #    ;;
-        #perf_dram)
-        #    echo "const int N_REGIONS = 4;" > config_shell.hpp
-        #    echo "const int EN_HLS = 0;" >> config_shell.hpp
-        #    echo "const int EN_STRM = 0;" >> config_shell.hpp
-        #    echo "const int EN_MEM = 1;" >> config_shell.hpp
-        #    echo "const int N_DDR_CHAN = 2;" >> config_shell.hpp
-        #    ;;
         perf_rdma_host)
             echo "const int EN_HLS = 0;" >> config_shell.hpp
             echo "const int EN_BPSS = 1;" >> config_shell.hpp
@@ -364,13 +301,6 @@ if ! [ -d "$DIR" ]; then
             echo "const int EN_MEM = 0;" >> config_shell.hpp
             echo "const int N_REGIONS = 1;" >> config_shell.hpp
             ;;
-        #perf_dram) 
-        #    echo "const int N_REGIONS = 4;" > config_shell.hpp
-        #    echo "const int EN_HLS = 0;" >> config_shell.hpp
-        #    echo "const int EN_STRM = 0;" >> config_shell.hpp
-        #    echo "const int EN_MEM = 1;" >> config_shell.hpp
-        #    echo "const int N_DDR_CHAN = 2;" >> config_shell.hpp
-        #    ;;
         *)
             echo ""
             echo "Unknown configuration."
@@ -502,32 +432,12 @@ if [ "$config_hw" = "perf_rdma_host" ]; then
     #run (local CPU)
     cd $DIR/build_dir.$FDEV_NAME.$vivado_version
     ./main
-    
-    #run (local CPU)
-    #cd $DIR/build_dir.$FDEV_NAME
-    #./main
-    #nohup ./main &
-    #tmux new-session -d -s main_session './main'
-
-    #run (remote CPUs)
-    #IFS=" " read -ra servers_family_list_array <<< "$servers_family_list"
-    #for i in "${servers_family_list_array[@]}"; do
-    #    ssh -t $USER@$i "cd $DIR/build_dir.$FDEV_NAME ; ./main -t $IP_address_cpu1_hex"
-    #    #ssh -t $USER@$i "cd $DIR/build_dir.$FDEV_NAME ; tmux send-keys -t main_session './main -t $IP_address_cpu1_hex' C-m"
-    #    #ssh -t jmoyapaya@alveo-u55c-06 "tmux send-keys -t main_session './main -t 10.253.74.82' C-m"
-    #done
-
-    #echo "For finishing your ${bold}perf_rdma_host validation:${normal} "
-    #echo "    1. On $hostname (this terminal) run:  ${bold}./main${normal}"
-    #echo "    2. On $servers_family_list (remote server) run: ${bold}./main -t $IP_address_cpu1${normal}"
-    #echo ""
 
 else
     #program
     sgutil program coyote --project $project_name --device $device_index --remote 0
     
     #run
-    #cd $MY_PROJECTS_PATH/$WORKFLOW/$project_name/build_dir.$FDEV_NAME
     cd $DIR/build_dir.$FDEV_NAME.$vivado_version
     ./main
 fi
