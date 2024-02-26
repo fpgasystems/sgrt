53293289289e2cfbd044b55f60d957693632b4e1
Javier Moya <jmoya@hey.com>
2024-02-26 16:45:38 +0100

    Cleanup

--- cli/new/vitis.sh
+++ cli/new/vitis.sh
@@ -7,6 +7,7 @@ normal=$(tput sgr0)
 CLI_PATH="$(dirname "$(dirname "$0")")"
 MY_PROJECTS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECTS_PATH)
 WORKFLOW="vitis"
+VITIS_COMMIT="d1a35c3"
 TEMPLATE_NAME="hello_world_xrt"
 
 # create my_projects directory
@@ -75,56 +76,8 @@ if ! [ -d "$DIR" ]; then
     exit
 fi
 
-#prepare for wget (1)
-if [ -d "$VITIS_DIR/common" ]; then
-    rm -rf "$VITIS_DIR/common"
-fi
-
-#prepare for wget (2)
-if [ -d "$VITIS_DIR/tmp" ]; then
-    rm -rf "$VITIS_DIR/tmp"
-fi
-
-VITIS_COMMIT="d1a35c3"
-
-#copy files
-#echo ""
-#echo "${bold}Checking out Vitis_Accel_Examples/common (commit: $VITIS_COMMIT):${normal}"
-#echo ""
-#wget https://github.com/Xilinx/Vitis_Accel_Examples/archive/master.zip -O $VITIS_DIR/master.zip
-#mkdir $VITIS_DIR/tmp
-#unzip -q $VITIS_DIR/master.zip -d $VITIS_DIR/tmp
-#mv -f $VITIS_DIR/tmp/Vitis_Accel_Examples-main/common $VITIS_DIR
-#rm -rf $VITIS_DIR/tmp
-#rm $VITIS_DIR/master.zip
-
-
-## Download the repository zip file for the specific commit
-#wget https://github.com/Xilinx/Vitis_Accel_Examples/archive/$VITIS_COMMIT.zip -O $VITIS_DIR/master.zip
-
-## Create a temporary directory
-#mkdir "$VITIS_DIR/tmp"
-
-## Unzip the downloaded file to the temporary directory
-#unzip -q "$VITIS_DIR/master.zip" -d "$VITIS_DIR/tmp"
-
-## Find the directory that matches the pattern Vitis_Accel_Examples-*
-#EXAMPLES_DIR=$(find "$VITIS_DIR/tmp" -maxdepth 1 -type d -name "Vitis_Accel_Examples-*")
-
-## Move the common directory to the desired location
-#mv -f "$EXAMPLES_DIR/common" "$VITIS_DIR"
-
-## Remove the temporary directory
-#rm -rf "$VITIS_DIR/tmp"
-
-## Remove the downloaded zip file
-#rm "$VITIS_DIR/master.zip"
-
-
 #clone Vitis_Accel_Examples/common
 $CLI_PATH/common/git_clone_vitis $VITIS_DIR $VITIS_COMMIT
-
-
 
 #copy template from SGRT_PATH
 SGRT_PATH=$(dirname "$CLI_PATH")
@@ -142,7 +95,6 @@ rm $DIR/README.rst
 #echo "# $project_name" >> README.md
 #compile config_add and delete
 cd $DIR/src
-#g++ -std=c++17 config_add.cpp -o ../config_add >&/dev/null ### to be removed!!!!!!!!!!!!
 mv $DIR/config_add.sh $DIR/config_add
 chmod +x $DIR/config_add
 mv $DIR/config_delete.sh $DIR/config_delete
