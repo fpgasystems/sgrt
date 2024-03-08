813cef70d0e3ceb873e3f3e82bdd82f49712e553
Javier Moya <jmoya@hey.com>
2024-03-08 05:54:24 +0100

    Cleanup

--- cli/program/enable_N_REGIONS.sh
+++ cli/program/enable_N_REGIONS.sh
@@ -7,16 +7,7 @@ normal=$(tput sgr0)
 CLI_PATH="$(dirname "$(dirname "$0")")"
 
 #inputs
-#DIR=$1
 device_index=$1
-#N_REGIONS=$3
-
-#get upstream_port
-#upstream_port=$($CLI_PATH/get/get_fpga_device_param $device_index upstream_port)
-
-#transform to bus_device (from a1:00.0 to a1_00)
-#bus_device=$(echo "$upstream_port" | sed 's/:/_/;s/\..*//')
-
 
 #get bus and device
 bus=$($CLI_PATH/program/get_bus_device $device_index bus)
@@ -24,15 +15,6 @@ device=$($CLI_PATH/program/get_bus_device $device_index device)
 
 #get N_REGIONS
 N_REGIONS=$(cat /sys/kernel/coyote_sysfs_${bus}_${device}/cyt_attr_cnfg | grep vFPGA | awk -F': ' '{print $2}')
-
-##get N_REGIONS
-#line=$(grep -n "N_REGIONS" $DIR/configs/config_shell_static)
-##find equal (=)
-#idx=$(sed 's/ /\n/g' <<< "$line" | sed -n "/=/=")
-##get index
-#value_idx=$(($idx+1))
-##get data
-#N_REGIONS=$(echo $line | awk -v i=$value_idx '{ print $i }' | sed 's/;//' )
 
 #apply fpga_chmod to N_REGIONS
 echo "${bold}Enabling vFPGA regions:${normal}"
