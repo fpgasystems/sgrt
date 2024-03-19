2e57af2bd4db563dc32e0368d0a8a548232bdec5
Javier Moya <jmoya@hey.com>
2024-03-19 08:37:16 +0100

    Adding compare_files

---
+++ cli/common/compare_files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+file1=$1
+file2=$2
+
+# Declare global variables
+declare -g equals="1"
+
+# Count the number of lines in each file
+lines_file1=$(wc -l < "$file1")
+lines_file2=$(wc -l < "$file2")
+
+# Compare the line counts
+if [ "$lines_file1" -ne "$lines_file2" ]; then
+    equals="0"
+else
+    while IFS= read -r line; do
+        # Check if the line exists in file2
+        grep -qF "$line" "$file2"
+        if [ $? -ne 0 ]; then
+            equals="0"
+            break
+        fi
+    done < "$file1"
+fi
+
+#return value
+echo "$equals"
