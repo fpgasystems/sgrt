fa05cb5bda3753b66d719ef2af09d9e9ba411f22
Javier Moya <jmoya@hey.com>
2024-03-22 14:06:56 +0100

    Adding get_xclbin_cfg

---
+++ cli/common/get_xclbin_cfg.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+nk_file=$1
+sp_file=$2
+output_dir=$3
+
+#read from nk
+declare -a xclbin_names
+declare -a compute_units_num
+declare -a compute_units_names
+
+while read -r line; do
+    column_1=$(echo "$line" | awk '{print $1}')
+    column_2=$(echo "$line" | awk '{print $2}')
+    column_3=$(echo "$line" | awk '{print $3}')
+    xclbin_names+=("$column_1")
+    compute_units_num+=("$column_2")
+    compute_units_names+=("$column_3")
+done < "$nk_file"
+
+#read from sp to create sp_aux (to append later)
+touch $output_dir/sp_aux
+while read -r line; do
+    # Extract second column (e.g., "vadd")
+    operation=$(echo "$line" | awk '{print $2}')
+    # Extract other columns except the first two
+    columns=$(echo "$line" | awk '{$1=""; $2=""; print $0}')
+    # Split the columns based on whitespace and iterate over them
+    for col in $columns; do
+        # Construct and print the desired output
+        echo "sp=$operation.$col" >> $output_dir/sp_aux
+    done
+done < "$sp_file"
+
+for ((i = 0; i < ${#xclbin_names[@]}; i++)); do
+
+    #map to nk
+    xclbin_i="${xclbin_names[i]}"
+    compute_units_num_i="${compute_units_num[i]}"
+    compute_units_names_i="${compute_units_names[i]}"
+
+    #create <xclbin.cfg>
+    touch $output_dir/$xclbin_i.cfg
+    echo "[connectivity]" >> $output_dir/$xclbin_i.cfg
+
+    #nk
+    if [ "$compute_units_names_i" = "" ]; then
+        echo "nk=$xclbin_i:$compute_units_num_i" >> $output_dir/$xclbin_i.cfg 
+    else
+        echo "nk=$xclbin_i:$compute_units_num_i:$compute_units_names_i" >> $output_dir/$xclbin_i.cfg 
+    fi
+
+    echo >> $output_dir/$xclbin_i.cfg
+
+    #sp
+    grep "sp=$xclbin_i" $output_dir/sp_aux >> $output_dir/$xclbin_i.cfg
+
+
+done
+
+#remove sp_aux
+if [ -f "$output_dir/sp_aux" ]; then
+    rm "$output_dir/sp_aux"
+fi
