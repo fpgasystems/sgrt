315404691234e5ef66a06aac05445bbb73c13154
Javier Moya <jmoya@hey.com>
2024-05-14 15:11:00 +0200

    Not needed as merged to api.hpp

--- api/host.hpp
+++
@@ -1,7 +0,0 @@
-#include "host/create_data.hpp"
-#include "host/get_project_path.hpp"
-#include "host/get_config.hpp"
-#include "host/get_target.hpp"
-#include "host/get_config_parameter.hpp"
-#include "host/open.hpp"
-#include "host/write.hpp"
