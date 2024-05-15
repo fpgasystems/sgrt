90bbbd96044f296b69bd71e8d5b37f768cfde6db
Javier Moya <jmoya@hey.com>
2024-05-15 16:20:26 +0200

    Fixes compilation

--- templates/vitis/hello_world_xrt/xclbin_add.sh
+++ templates/vitis/hello_world_xrt/xclbin_add.sh
@@ -7,7 +7,8 @@ normal=$(tput sgr0)
 MY_PROJECT_PATH="$(dirname "$(dirname "$0")")"
 
 #get template (TEMPLATES_PATH is an environment variable)
-TEMPLATE="$TEMPLATES_PATH/vitis/hello_world_xrt/src/xclbin/vadd.cpp" 
+XCLBIN_NAME="vadd"
+TEMPLATE="$TEMPLATES_PATH/vitis/hello_world_xrt/src/xclbin/$XCLBIN_NAME.cpp" 
 
 # create project
 echo ""
@@ -26,6 +27,9 @@ done
 
 #copy template
 cp $TEMPLATE $MY_PROJECT_PATH/src/xclbin/$xclbin_name.cpp
+
+#replace XCLBIN_NAME with $xclbin_name 
+sed -i "s/$XCLBIN_NAME/$xclbin_name/g" "$MY_PROJECT_PATH/src/xclbin/$xclbin_name.cpp"
 
 #add to nk
 echo "$xclbin_name 1 ${xclbin_name}_a" >> $MY_PROJECT_PATH/nk
