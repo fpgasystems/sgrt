8725ba9868afa496dd49c4cdd6c7df4b3908efbd
Javier Moya <jmoya@hey.com>
2024-05-21 09:33:55 +0200

    reading devices and xclbin_names

--- templates/vitis/hello_world_xrt/src/host.cpp
+++ templates/vitis/hello_world_xrt/src/host.cpp
@@ -59,8 +59,13 @@ int main(int argc, char** argv) {
 
     // get sp (Synthesis and Implementation Process) file
     std::vector<std::string> devices = host::get_sp("devices");
+    std::vector<std::string> xclbin_names = host::get_sp("xclbin_names");
     for (const std::string& device : devices) {
         std::cout << device << std::endl;
+    }
+
+    for (const std::string& xclbin_name : xclbin_names) {
+        std::cout << xclbin_name << std::endl;
     }
 
     // open devices
