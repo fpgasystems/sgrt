ea07a4731b56db78f024f8388067bab9bfdcf79b
Javier Moya <jmoya@hey.com>
2024-06-17 09:53:15 +0200

    New help files

---
+++ cli/help/program_revert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+#echo ""
+echo "${bold}sgutil program revert [flags] [--help]${normal}"
+echo ""
+echo "Returns the specified FPGA to the Vitis workflow."
+echo ""
+echo "FLAGS:"
+echo "   -d, --device    - FPGA Device Index (see sgutil examine)."
+echo ""
+echo "   -h, --help      - Help to revert a device."
+echo ""
