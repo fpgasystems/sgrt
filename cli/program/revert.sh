a8ff2d23b96a7a78424c6c356a83154cee23b526
Javier Moya <jmoya@hey.com>
2024-06-19 14:32:40 +0200

    Cleanup

--- cli/program/revert.sh
+++ cli/program/revert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CLI_PATH="$(dirname "$(dirname "$0")")"
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -11,88 +12,12 @@ device_index=$2
 vivado_version=$4
 
 #constants
-CLI_PATH="$(dirname "$(dirname "$0")")"
-XILINX_TOOLS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH XILINX_TOOLS_PATH)
-VIVADO_PATH="$XILINX_TOOLS_PATH/Vivado"
-DEVICES_LIST="$CLI_PATH/devices_acap_fpga"
 MY_DRIVERS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_DRIVERS_PATH)
 SERVERADDR="localhost"
+XILINX_TOOLS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH XILINX_TOOLS_PATH)
 
-#echo $device_index
-#echo $vivado_version
-
-#echo "adeu"
-
-#exit
-
-#get username
-#username=$USER
-
-#get hostname
-#url="${HOSTNAME}"
-#hostname="${url%%.*}"
-
-#check on virtualized
-#virtualized=$($CLI_PATH/common/is_virtualized $CLI_PATH $hostname)
-#if [ "$virtualized" = "1" ]; then
-#    echo ""
-#    echo "Sorry, this command is not available on ${bold}$hostname!${normal}"
-#    echo ""
-#    exit
-#fi
-
-#check on ACAP or FPGA servers (server must have at least one ACAP or one FPGA)
-#acap=$($CLI_PATH/common/is_acap $CLI_PATH $hostname)
-#fpga=$($CLI_PATH/common/is_fpga $CLI_PATH $hostname)
-#if [ "$acap" = "0" ] && [ "$fpga" = "0" ]; then
-#    echo ""
-#    echo "Sorry, this command is not available on ${bold}$hostname!${normal}"
-#    echo ""
-#    exit
-#fi
-
-#get Vivado version
-#vivado_version=$(find "$VIVADO_PATH" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
-
-#check on valid Vivado version (using $XILINX_VIVADO is not possible)
-#if [ ! -d $VIVADO_PATH/$vivado_version ]; then
-#    echo ""
-#    echo "Please, source a valid Vivado version for ${bold}$hostname!${normal}"
-#    echo ""
-#    exit 1
-#fi
-
-#check on valid Vivado version
-#vivado_version=$($CLI_PATH/common/get_xilinx_version vivado)
-
-#if [ -z "$vivado_version" ]; then #if [ -z "$(echo $vivado_version)" ]; then
-#    echo ""
-#    echo "Please, source a valid Vivado version for ${bold}$hostname!${normal}"
-#    echo ""
-#    exit 1
-#fi
-
-#check on DEVICES_LIST
-#source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST"
-
-#get number of fpga and acap devices present
-#MAX_DEVICES=$(grep -E "fpga|acap" $DEVICES_LIST | wc -l)
-
-#echo $MAX_DEVICES
-
-#MAX_DEVICES=$($CLI_PATH/common/get_max_devices "fpga|acap" $DEVICES_LIST)
-
-#echo $MAX_DEVICES
-
-#check on multiple devices
-#multiple_devices=$($CLI_PATH/common/get_multiple_devices $MAX_DEVICES)
-
-#inputs
-#read -a flags <<< "$@"
-
-#version_dialog_check
-#result="$("$CLI_PATH/common/version_dialog_check" "${flags[@]}")"
-#vivado_version=$(echo "$result" | sed -n '2p')
+#derived
+VIVADO_PATH="$XILINX_TOOLS_PATH/Vivado"
 
 #check on Vivado version
 if [ -n "$vivado_version" ]; then
@@ -115,42 +40,6 @@ else
         exit 1
     fi
 fi
-
-#check on flags
-#device_found=""
-#device_index=""
-#if [ "$flags" = "" ]; then
-#    #device_dialog
-#    if [[ $multiple_devices = "0" ]]; then
-#        device_found="1"
-#        device_index="1"
-#    else
-#        echo ""
-#        echo "${bold}Please, choose your device:${normal}"
-#        echo ""
-#        result=$($CLI_PATH/common/device_dialog $CLI_PATH $MAX_DEVICES $multiple_devices)
-#        device_found=$(echo "$result" | sed -n '1p')
-#        device_index=$(echo "$result" | sed -n '2p')
-#    fi
-#else
-#    #device_dialog_check
-#    result="$("$CLI_PATH/common/device_dialog_check" "${flags[@]}")"
-#    device_found=$(echo "$result" | sed -n '1p')
-#    device_index=$(echo "$result" | sed -n '2p')
-#    #forbidden combinations
-#    if ([ "$device_found" = "1" ] && [ "$device_index" = "" ]) || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))) || ([ "$device_found" = "1" ] && ([[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]])); then
-#        $CLI_PATH/help/program_revert
-#        exit
-#    fi
-#    #device_dialog (forgotten mandatory)
-#    if [[ $multiple_devices = "0" ]]; then
-#        device_found="1"
-#        device_index="1"
-#    elif [[ $device_found = "0" ]]; then
-#        $CLI_PATH/help/program_revert
-#        exit
-#    fi
-#fi
 
 #check on workflow
 workflow=$($CLI_PATH/common/get_workflow $CLI_PATH $device_index)
@@ -216,7 +105,6 @@ sudo $CLI_PATH/program/pci_hot_plug 1 $upstream_port $root_port $LinkCtl
 #inserting XRT driver
 echo "${bold}Inserting XRT drivers:${normal}"
 echo ""
-
 if [[ $(lsmod | grep xocl | wc -l) -gt 0 ]]; then
     echo "sudo modprobe xocl"
     sudo modprobe xocl
@@ -228,5 +116,3 @@ if [[ $(lsmod | grep xclmgmt | wc -l) -gt 0 ]]; then
     sleep 1
 fi
 echo ""
-#lspci | grep Xilinx | grep $bdf
-#echo ""
