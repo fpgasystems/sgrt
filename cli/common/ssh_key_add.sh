421d2a2b86ef0b35f180e73ed4b8d77e24d7abd5
Javier Moya <jmoya@hey.com>
2024-06-25 06:37:24 +0200

    Check on server list

--- cli/common/ssh_key_add.sh
+++ cli/common/ssh_key_add.sh
@@ -8,6 +8,7 @@ servers=("${@:2}")
 
 # create key
 echo "${bold}Creating id_rsa private and public keys:${normal}"
+echo ""
 FILE="/home/$USER/.ssh/id_rsa.pub"
 if ! [ -f "$FILE" ]; then
     #create key
@@ -28,20 +29,21 @@ else
     echo ""
 fi
 
-## add SSH (mellanox-0) fingerprints to local known_hosts
-echo "${bold}Adding fingerprints to known_hosts:${normal}"
-echo ""
+if [ ${#servers[@]} -ne 0 ]; then
+    ## add SSH (mellanox-0) fingerprints to local known_hosts
+    echo "${bold}Adding fingerprints to known_hosts:${normal}"
+    
+    #alveo-build-01
+    #ssh-keygen -R alveo-build-01-mellanox-0
+    #ssh-keyscan -H alveo-build-01-mellanox-0 >> ~/.ssh/known_hosts #> /dev/null
 
-#alveo-build-01
-#ssh-keygen -R alveo-build-01-mellanox-0
-#ssh-keyscan -H alveo-build-01-mellanox-0 >> ~/.ssh/known_hosts #> /dev/null
-
-#booked servers
-for i in "${servers[@]}"
-do
+    #booked servers
+    for i in "${servers[@]}"
+    do
+        echo ""
+        ssh-keygen -R $i-mellanox-0
+        ssh-keyscan -H $i-mellanox-0 >> ~/.ssh/known_hosts #> /dev/null
+        sleep 1
+    done
     echo ""
-    ssh-keygen -R $i-mellanox-0
-    ssh-keyscan -H $i-mellanox-0 >> ~/.ssh/known_hosts #> /dev/null
-    sleep 1
-done
-echo ""
+fi
