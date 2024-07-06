0109fa1b55e578c5a7ddd9122c13203dd73b41b8
Javier Moya <jmoya@hey.com>
2024-07-06 09:18:36 +0200

    Fix

--- cli/common/gh_commit_check.sh
+++ cli/common/gh_commit_check.sh
@@ -8,14 +8,13 @@ COMMIT_ID=$3
 #constants
 COMMIT_ID_LENGTH=7
 
+exists=""
+
 #check on COMMIT_ID string length
 if [ ${#COMMIT_ID} -lt $COMMIT_ID_LENGTH ]; then
-    echo ""
-    echo "Your commit ID must have at least seven characters."
-    echo ""
-    exit 1
+    exists="0"
+else
+    exists=$($GITHUB_CLI_PATH/gh api repos/$GITHUB_REPO/commits/$COMMIT_ID 2>/dev/null | jq -r 'if has("sha") then "1" else "0" end')
 fi
-
-exists=$($GITHUB_CLI_PATH/gh api repos/$GITHUB_REPO/commits/$COMMIT_ID 2>/dev/null | jq -r 'if has("sha") then "1" else "0" end')
 
 echo "$exists"
