301b30c98e60305ce2ff82ffd13c79d933ff47c2
Javier Moya <jmoya@hey.com>
2024-07-19 16:10:31 +0200

    Working version

--- cli/update.sh
+++ cli/update.sh
@@ -19,13 +19,24 @@ local_commit_date=$(cat $BASE_PATH/COMMIT_DATE)
 remote_timestamp=$(date -d "$remote_commit_date" +%s)
 local_timestamp=$(date -d "$local_commit_date" +%s)
 
-#compare the timestamps
+#compare the timestamps and confirm update
 update="0"
 if [ "$local_timestamp" -lt "$remote_timestamp" ]; then
     echo ""
     echo "${bold}This will update $CLI_NAME to its latest version. Would you like to continue (y/n)?${normal}"
     update=$($CLI_PATH/common/push_dialog)
     echo ""
+fi
+
+
+if [ $update = "1" ]; then
+
+  echo "We will checkout and update" 
+
+  #update COMMIT and COMMIT_DATE
+
+  #remove from temporal UPDATE_PATH
+
 fi
 
 echo "$remote_commit_date"
