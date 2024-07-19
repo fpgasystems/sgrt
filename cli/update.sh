4211997bed5a62283397a0168e7d29558451a19d
Javier Moya <jmoya@hey.com>
2024-07-19 15:59:35 +0200

    Dialog works

--- cli/update.sh
+++ cli/update.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 CLI_PATH="$(dirname "$0")"
+CLI_NAME="sgrt"
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 #constants
 BASE_PATH=$(dirname "$CLI_PATH")
 MAIN_BRANCH_URL="https://api.github.com/repos/fpgasystems/sgrt/commits/main"
-
-
-#only for sudo =====> at sgutil level
 
 #get last commit date on the remote
 remote_commit_date=$(curl -s $MAIN_BRANCH_URL | jq -r '.commit.committer.date')
@@ -22,12 +20,14 @@ remote_timestamp=$(date -d "$remote_commit_date" +%s)
 local_timestamp=$(date -d "$local_commit_date" +%s)
 
 #compare the timestamps
-update_required="0"
+update="0"
 if [ "$local_timestamp" -lt "$remote_timestamp" ]; then
-    echo "The local commit date is anterior to the remote commit date."
-    update_required="1"
+    echo ""
+    echo "${bold}This will update $CLI_NAME to its latest version. Would you like to continue (y/n)?${normal}"
+    update=$($CLI_PATH/common/push_dialog)
+    echo ""
 fi
 
 echo "$remote_commit_date"
 echo "$local_commit_date"
-echo "$update_required"
+echo "$update"
