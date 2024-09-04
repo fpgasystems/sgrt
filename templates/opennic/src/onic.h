a93b1d89a1a5bc483e99cbfe2c643747b28d44a5
Javier Moya <jmoya@hey.com>
2024-09-04 05:35:33 +0200

    Reorder cleanup

--- templates/opennic/src/onic.h
+++ templates/opennic/src/onic.h
@@ -1,8 +1,8 @@
 #ifndef ONIC_H
 #define ONIC_H
 
+void flags_check(int argc, char *argv[], char **onic_name, char **remote_server_name, int *index);
 void ping(const char *onic_name, const char *remote_server_name, int num_pings);
 int read_parameter(int index, const char *parameter_name);
-void flags_check(int argc, char *argv[], char **onic_name, char **remote_server_name, int *index);
 
 #endif
