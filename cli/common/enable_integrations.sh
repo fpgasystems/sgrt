0fd29a42ac2e0638867d0fd659c2ddb16b711e1d
Javier Moya <jmoya@hey.com>
2024-09-24 12:51:46 +0200

    Add enable_integrations

---
+++ cli/common/enable_integrations.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+workflow=$1
+is_acap=$2
+is_build=$3
+is_fpga=$4
+is_vivado_developer=$5
+
+#integrations
+gpu_integrations="0"
+vivado_integrations="0"
+
+#return
+if [ "$workflow" = "gpu" ]; then
+    #gpu
+    if [ "$is_build" = "1" ] || [ "$is_gpu" = "1" ]; then
+        gpu_integrations="1"
+    fi
+    echo "$gpu_integrations"
+elif [ "$workflow" = "vivado" ]; then
+    #vivado
+    if [ "$is_vivado_developer" = "1" ] && { [ "$is_acap" = "1" ] || [ "$is_build" = "1" ] || [ "$is_fpga" = "1" ]; }; then
+        vivado_integrations="1"
+    fi
+    echo "$vivado_integrations"
+fi
