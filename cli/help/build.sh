1fea563749bc9d114be1f71208557dae06d1fcdd
Javier Moya <jmoya@hey.com>
2024-09-24 07:55:12 +0200

    Fix for is_build

--- cli/help/build.sh
+++ cli/help/build.sh
@@ -18,8 +18,8 @@ COLOR_OFF=$($CLI_PATH/common/get_constant $CLI_PATH COLOR_OFF)
 #build_commands="c"
 
 #integrations
-gpu_integration="0"
-vivado_integration="0"
+gpu_integrations="0"
+vivado_integrations="0"
 
 #print help
 echo ""
@@ -31,25 +31,37 @@ echo "ARGUMENTS:"
 echo "   ${bold}c${normal}               - Generates C and C++ binaries."
 if [ "$is_build" = "1" ] || [ "$is_gpu" = "1" ]; then
     echo -e "   ${bold}${COLOR_ON5}hip${normal}${COLOR_OFF}             ${COLOR_ON5}- Generates HIP binaries for your projects.${COLOR_OFF}"  
-    gpu_integration="1"
+    gpu_integrations="1"
 fi
 if [ "$is_vivado_developer" = "1" ] && { [ "$is_acap" = "1" ] || [ "$is_build" = "1" ] || [ "$is_fpga" = "1" ]; }; then
     echo -e "   ${bold}${COLOR_ON2}opennic${COLOR_OFF}${normal}         ${COLOR_ON2}- Generates OpenNIC's bitstreams and drivers.${COLOR_OFF}"
-    vivado_integration="1"
+    vivado_integrations="1"
 fi
 echo ""
 echo "   ${bold}-h, --help${normal}      - Help to use this command."
 echo ""
 
 #legend
-if [ "$gpu_integration" = "1" ] && [ "$vivado_integration" = "1" ]; then
-    $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "$is_acap" "$is_fpga" "$is_gpu"
+if [ "$gpu_integrations" = "1" ] && [ "$vivado_integrations" = "1" ]; then
+    if [ "$is_build" = "1" ]; then
+        $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "1" "1" "1"
+    else
+        $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "$is_acap" "$is_fpga" "$is_gpu"
+    fi
     #echo ""
-elif [ "$gpu_integration" = "0" ] && [ "$vivado_integration" = "1" ]; then
-    $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "$is_acap" "$is_fpga" "0"
+elif [ "$gpu_integrations" = "0" ] && [ "$vivado_integrations" = "1" ]; then
+    if [ "$is_build" = "1" ]; then
+        $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "1" "1" "0"
+    else
+        $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "$is_acap" "$is_fpga" "0"
+    fi
     #echo ""
-elif [ "$gpu_integration" = "1" ] && [ "$vivado_integration" = "0" ]; then
-    $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "0" "0" "1"
+elif [ "$gpu_integrations" = "1" ] && [ "$vivado_integrations" = "0" ]; then
+    if [ "$is_build" = "1" ]; then
+        $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "0" "0" "1"
+    else
+        $CLI_PATH/common/print_legend "$CLI_PATH" "$CLI_NAME" "0" "0" "1"
+    fi
     #echo ""
 fi
 echo ""
