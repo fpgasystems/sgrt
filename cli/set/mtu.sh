0743fd1e22bad39127e28c79c1c89abb713b5582
Javier Moya <jmoya@hey.com>
2024-09-27 11:31:33 +0200

    Fix mtu

--- cli/set/mtu.sh
+++ cli/set/mtu.sh
@@ -10,6 +10,7 @@ MTU_MAX=9000
 MTU_DEFAULT=1576 # (1576 - 40) / 64 = 24
 IPV6_HEADER_SIZE=40
 PAYLOAD_MULTIPLES=64
+CHECK_ON_MTU_ERR_MSG="Please, choose a valid MTU value."
 
 #get hostname
 #url="${HOSTNAME}"
@@ -43,36 +44,46 @@ fi
 read -a flags <<< "$@"
 
 #check on flags
-mtu_valid="0"
+mtu_found="0"
+mtu_value=""
 if [ "$flags" = "" ]; then
-    mtu_valid="1"
-    mtu=$MTU_DEFAULT
+    #mtu_found="1"
+    #mtu_value=$MTU_DEFAULT
+    echo ""
+    echo "$CHECK_ON_MTU_ERR_MSG"
+    echo ""
+    exit
 else
     if [[ " ${flags[0]} " =~ " -v " ]] || [[ " ${flags[0]} " =~ " --value " ]]; then
-        mtu_valid="1"
-        mtu=${flags[1]}
+        mtu_found="1"
+        mtu_value=${flags[1]}
     fi
 fi
 
-if [ "$mtu_valid" = "1" ]; then
+#forbidden combinations
+if [ "$mtu_found" = "1" ] && [ "$mtu_value" = "" ]; then
+    $CLI_PATH/sgutil set mtu -h
+    exit
+fi
 
+if [ "$mtu_found" = "1" ]; then
     #get closest MTU for a payload multiple
-    mtu=$(calculate_closest_mtu $mtu $IPV6_HEADER_SIZE $PAYLOAD_MULTIPLES)
+    mtu_value=$(calculate_closest_mtu $mtu_value $IPV6_HEADER_SIZE $PAYLOAD_MULTIPLES)
 
     #verify MTU is between valid range
-    if [ "$mtu" -lt "$MTU_MIN" ] || [ "$mtu" -gt "$MTU_MAX" ]; then
-        mtu=$MTU_DEFAULT
+    if [ "$mtu_value" -lt "$MTU_MIN" ] || [ "$mtu_value" -gt "$MTU_MAX" ]; then
+        mtu_value=$MTU_DEFAULT
     fi
 
     #get Mellanox name
     mellanox_name=$(nmcli dev | grep mellanox-0 | awk '{print $1}')
 
-    #set mtu
-    sudo ifconfig $mellanox_name mtu $mtu up
+    #set mtu_value
+    sudo ifconfig $mellanox_name mtu $mtu_value up
 
     #print message
     echo ""
-    echo "$mellanox_name MTU was set to $mtu bytes!"
+    echo "$mellanox_name MTU was set to $mtu_value bytes!"
     echo ""
 
 fi
