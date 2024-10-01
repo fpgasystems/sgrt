7af9f0248f5746b3311952d371c982c09dc0a239
Javier Moya <jmoya@hey.com>
2024-10-01 10:28:03 +0200

    Working version

--- cli/get/get_nic_config.sh
+++ cli/get/get_nic_config.sh
@@ -4,6 +4,29 @@
 CLI_PATH="$(dirname "$(dirname "$0")")"
 DEVICES_LIST="$CLI_PATH/devices_network"
 
+#helper functions
+split_addresses (){
+  #input parameters
+  str_ip=$1
+  str_mac=$2
+  aux=$3
+  #save the current IFS
+  OLDIFS=$IFS
+  #set the IFS to / to split the string at each /
+  IFS="/"
+  #read the two parts of the string into variables
+  read ip0 ip1 <<< "$str_ip"
+  read mac0 mac1 <<< "$str_mac"
+  #reset the IFS to its original value
+  IFS=$OLDIFS
+  #print the two parts of the string
+  if [[ "$aux" == "0" ]]; then
+    echo "$ip0 ($mac0)"
+  else
+    echo "$ip1 ($mac1)"
+  fi
+}
+
 #constants (id upstream_port root_port LinkCtl device_type device_name serial_number IP MAC)
 #ID_COLUMN=1
 #BDF_COLUMN=2
@@ -14,13 +37,19 @@ DEVICES_LIST="$CLI_PATH/devices_network"
 
 #inputs (./examine 0 root_port)
 device_index=$1
-parameter=$2
+port_index=$2
+parameter=$3
 
-#get device IP
+#get device addresses
 IP=$($CLI_PATH/get/get_nic_device_param $device_index IP)
-
-#get device MAC
 MAC=$($CLI_PATH/get/get_nic_device_param $device_index MAC)
+if [ "$port_index" = "1" ]; then
+  IP="${IP%%/*}"
+  MAC="${MAC%%/*}"
+elif [ "$port_index" = "2" ]; then
+  IP="${IP#*/}"
+  MAC="${MAC#*/}"
+fi
 
 case "$parameter" in
     # id upstream_port root_port LinkCtl device_type device_name serial_number IP MAC  
