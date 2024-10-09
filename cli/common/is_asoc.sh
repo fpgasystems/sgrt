d9872f78749422180db0d92cc7e5d0215425114b
Javier Moya <jmoya@hey.com>
2024-10-09 13:10:13 +0200

    Updating LIST

--- cli/common/is_asoc.sh
+++ cli/common/is_asoc.sh
@@ -5,13 +5,13 @@ CLI_PATH=$1
 hostname=$2
 
 #constants
-ACAP_SERVERS_LIST="$CLI_PATH/constants/ACAP_SERVERS_LIST"
+ASOC_SERVERS_LIST="$CLI_PATH/constants/ASOC_SERVERS_LIST"
 
-#check for acap
-acap="0"
-if (grep -q "^$hostname$" $ACAP_SERVERS_LIST); then
-    acap="1"
+#check for asoc
+asoc="0"
+if (grep -q "^$hostname$" $ASOC_SERVERS_LIST); then
+    asoc="1"
 fi
 
 #output
-echo $acap
+echo $asoc
