faf79a40d7a1f85d7e1bd8b1f3bbcc4a21c2ca90
Javier Moya <jmoya@hey.com>
2024-10-15 11:58:08 +0200

    Fix: Match completion

--- cli/new/hip.sh
+++ cli/new/hip.sh
@@ -5,8 +5,13 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 #early exit
+url="${HOSTNAME}"
+hostname="${url%%.*}"
+is_build=$($CLI_PATH/common/is_build $CLI_PATH $hostname)
+is_gpu=$($CLI_PATH/common/is_gpu $CLI_PATH $hostname)
 IS_GPU_DEVELOPER="1"
-if [ "$IS_GPU_DEVELOPER" = "0" ]; then
+gpu_enabled=$([ "$IS_GPU_DEVELOPER" = "1" ] && [ "$is_gpu" = "1" ] && echo 1 || echo 0)
+if [ "$is_build" = "0" ] && [ "$gpu_enabled" = "0" ]; then
     exit 1
 fi
 
