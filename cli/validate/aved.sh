d1da26514aa14dbab6dce6f9f9dd95fcf62ce591
Javier Moya <jmoya@hey.com>
2024-10-16 18:05:27 +0200

    Cleanup

--- cli/validate/aved.sh
+++ cli/validate/aved.sh
@@ -28,44 +28,19 @@ if [ "$device_index" = "" ]; then
     exit
 fi
 
-#constants
-ASOC_SERVERS_LIST="$CLI_PATH/constants/ASOC_SERVERS_LIST"
-
-#BITSTREAM_NAME=$($CLI_PATH/common/get_constant $CLI_PATH ONIC_SHELL_NAME)
-#BITSTREAMS_PATH="$CLI_PATH/bitstreams"
-#BUILD_SERVERS_LIST="$CLI_PATH/constants/BUILD_SERVERS_LIST"
-COLOR_FAILED=$($CLI_PATH/common/get_constant $CLI_PATH COLOR_FAILED)
-COLOR_OFF=$($CLI_PATH/common/get_constant $CLI_PATH COLOR_OFF)
-COLOR_PASSED=$($CLI_PATH/common/get_constant $CLI_PATH COLOR_PASSED)
-#GPU_SERVERS_LIST="$CLI_PATH/constants/GPU_SERVERS_LIST"
-#DEPLOY_OPTION="0"
-#DRIVER_NAME=$($CLI_PATH/common/get_constant $CLI_PATH ONIC_DRIVER_NAME)
-#FPGA_SERVERS_LIST="$CLI_PATH/constants/FPGA_SERVERS_LIST"
-#MY_DRIVERS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_DRIVERS_PATH)
-#MY_PROJECTS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECTS_PATH)
-#NUM_PINGS="5"
-#SERVERADDR="localhost"
-#WORKFLOW="opennic"
-#XILINX_TOOLS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH XILINX_TOOLS_PATH)
-
-#derived
-DEVICES_LIST="$CLI_PATH/devices_acap_fpga"
-
-echo "HERE!"
-
 #get device_name
 upstream_port=$($CLI_PATH/get/get_fpga_device_param $device_index upstream_port)
 
 echo "upstream_port: $upstream_port"
-exit
 
 #ami_tool validation
 ami_tool overview
-ami_tool mfg_info -d $upstream_port
 ami_tool mfg_info -d $upstream_port
 
 #xbtest validation
 xbtest -d $upstream_port -c verify
 xbtest -d $upstream_port -c memory
+
+echo ""
 
 #author: https://github.com/jmoya82
