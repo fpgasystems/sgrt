7f97c152d11fa1e139e5b05e402090d0b6bd2d54
Javier Moya <jmoya@hey.com>
2024-10-19 15:16:02 +0200

    Cleanup

--- cli/get/partitions.sh
+++ cli/get/partitions.sh
@@ -12,22 +12,14 @@ normal=$(tput sgr0)
 url="${HOSTNAME}"
 hostname="${url%%.*}"
 is_asoc=$($CLI_PATH/common/is_asoc $CLI_PATH $hostname)
-#is_vivado_developer=$($CLI_PATH/common/is_member $USER vivado_developers)
-#vivado_enabled_asoc=$([ "$is_vivado_developer" = "1" ] && [ "$is_asoc" = "1" ] && echo 1 || echo 0)
 if [ "$is_asoc" = "0" ]; then
     exit
 fi
-
-echo "Hey I am here"
-
 
 #inputs
 device_index=$2
 
 #constants
-#AVED_PATH=$($CLI_PATH/common/get_constant $CLI_PATH AVED_PATH)
-#AVED_TAG=$($CLI_PATH/common/get_constant $CLI_PATH AVED_TAG)
-#AVED_UUID=$($CLI_PATH/common/get_constant $CLI_PATH AVED_UUID)
 DEVICES_LIST="$CLI_PATH/devices_acap_fpga"
 TYPE="primary"
 
@@ -49,7 +41,7 @@ if [ "$device_index" = "" ]; then
             partitions=$(ami_tool cfgmem_info -d $upstream_port -t $TYPE | awk '/^Partition/ {flag=1; next} flag && /^[0-9]/' | wc -l)
             partitions=$((partitions - 1))
             #print
-            echo "$device_index: 0 ... $partitions"
+            echo "$device_index: [0 ... $partitions]"
         else
             #print
             echo "$device_index: "
@@ -61,9 +53,9 @@ else
     partitions=$(ami_tool cfgmem_info -d $upstream_port -t $TYPE | awk '/^Partition/ {flag=1; next} flag && /^[0-9]/' | wc -l)
     partitions=$((partitions - 1))
     #print
+    echo ""
     echo "$device_index: 0 ... $partitions"
+    echo ""
 fi
-
-#exit
 
 #author: https://github.com/jmoya82
