8252f618bd60c01615cd4ca928db6178a4713bb4
Javier Moya <jmoya@hey.com>
2024-10-22 11:47:38 +0200

    Fix

--- cli/common/file_path_dialog_check.sh
+++ cli/common/file_path_dialog_check.sh
@@ -9,7 +9,7 @@ declare -g file_path=""
 #read flags
 for (( i=0; i<${#flags[@]}; i++ ))
 do
-    if [[ " ${flags[$i]} " =~ " -b " ]] || [[ " ${flags[$i]} " =~ " --bitstream " ]]; then # flags[i] is -d or --device
+    if [[ " ${flags[$i]} " =~ " -p " ]] || [[ " ${flags[$i]} " =~ " --path " ]]; then # flags[i] is -d or --device
         file_path_found="1"
         idx=$(($i+1))
         file_path=${flags[$idx]}
