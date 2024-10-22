37c2a168767b95b2e3e62847c141403e5ddc8685
Javier Moya <jmoya@hey.com>
2024-10-22 09:47:55 +0200

    file_path_dialog_check add

---
+++ cli/common/file_path_dialog_check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+flags=("$@")  # Assign command-line arguments to the 'flags' array
+
+# Declare global variables
+declare -g file_path_found="0"
+declare -g file_path=""
+
+#read flags
+for (( i=0; i<${#flags[@]}; i++ ))
+do
+    if [[ " ${flags[$i]} " =~ " -b " ]] || [[ " ${flags[$i]} " =~ " --bitstream " ]]; then # flags[i] is -d or --device
+        file_path_found="1"
+        idx=$(($i+1))
+        file_path=${flags[$idx]}
+    fi  
+done
+
+#return the values
+echo "$file_path_found"
+echo "$file_path"
