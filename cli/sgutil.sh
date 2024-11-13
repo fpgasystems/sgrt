b1e39fc607dd3b2f4287cf705a197a5f262a79cc
Javier Moya <jmoya@hey.com>
2024-11-13 07:58:14 +0100

    topo prints legend too

--- cli/sgutil.sh
+++ cli/sgutil.sh
@@ -2224,6 +2224,23 @@ case "$command" in
       topo)
         valid_flags="-h --help"
         command_run $command_arguments_flags"@"$valid_flags
+        #is_acap=$($CLI_PATH/common/is_acap $CLI_PATH $hostname)
+        #is_asoc=$($CLI_PATH/common/is_asoc $CLI_PATH $hostname)
+        #is_fpga=$($CLI_PATH/common/is_fpga $CLI_PATH $hostname)
+        #is_gpu=$($CLI_PATH/common/is_gpu $CLI_PATH $hostname)
+        #legend
+        legend="${legend}${bold}${COLOR_ON1}NICs${COLOR_OFF}${normal}"
+        if [ "$is_acap" = "1" ] || [ "$is_asoc" = "1" ] || [ "$is_fpga" = "1" ]; then
+          legend="${legend} ${bold}${COLOR_ON2}Adaptive Devices${COLOR_OFF}${normal}"
+        fi
+        if [ "$is_gpu" = "1" ]; then
+          legend="${legend} ${bold}${COLOR_ON5}GPUs${COLOR_OFF}${normal}"
+        fi
+        #print legend
+        if [[ -n "$legend" ]]; then
+          echo -e "$legend"
+          echo ""
+        fi
         ;;
       workflow)
         #early exit
